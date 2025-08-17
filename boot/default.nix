160804af36d295f01cd4f257dcc2c51216e429da
acmota2 <acmota2@gmail.com>
2025-08-17 02:08:41 +0200

    fix(boot): bootloader

--- boot/default.nix
+++ boot/default.nix
@@ -1,17 +1,7 @@
 {
-  boot = {
-    loader = {
-      efi.canTouchEfiVariables = true;
-      grub = {
-        enable = true;
-        efiSupport = true;
-        device = "nodev";
-      };
-    };
-    plymouth = {
-      enable = true;
-      theme = "bgrt";
-    };
-    resumeDevice = "/dev/disk/by-uuid/4d4efbda-9f85-42a0-adf0-4c60d9f6d6e6";
+  boot.loader.grub = {
+    enable = true;
+    device = "/dev/sda";
+    useOSProber = true;
   };
 }
